7cc3d4e6667248b96d248d367f8d1b7075eaa20c
stanleydukor <stanleydukor@gmail.com>
2023-06-29 03:21:25 -0700

    separate components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
-import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { useLoader } from "@react-three/fiber";
-import {
-  Group,
-  TextureLoader,
-  RawShaderMaterial,
-  DoubleSide,
-  GLSL3,
-  Mesh,
-  NearestFilter,
-  OrthographicCamera,
-  Scene,
-  PlaneGeometry,
-  Color,
-  WebGLMultipleRenderTargets,
-  LinearFilter,
-  FloatType,
-} from "three";
-import { OrbitControls } from "@react-three/drei";
-import { vertexShader, fragmentShader, renderer } from "./components/shaders";
-import createViewDependenceFunctions from "./components/createViewDependence";
-import createNetworkWeightTexture from "./components/createNetworkWeightTexture";
+import React, { useState, useEffect } from "react";
 import { getFileTypes } from "./components/utils";
 import MIC from "./config/mic";
 import AppStyle from "./style";
@@ -41,131 +18,7 @@ import Switch from "@mui/material/Switch";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
-
-extend({ OBJLoader });
-
-function PostProcessingScene({ jsonFile, params, renderTargetRef }) {
-  const { gl, scene, camera } = useThree();
-  const network_weights = jsonFile;
-  const fragmentShaderSource = createViewDependenceFunctions(network_weights);
-  const weightsTexZero = createNetworkWeightTexture(
-    network_weights["0_weights"]
-  );
-  const weightsTexOne = createNetworkWeightTexture(
-    network_weights["1_weights"]
-  );
-  const weightsTexTwo = createNetworkWeightTexture(
-    network_weights["2_weights"]
-  );
-
-  // Create a new scene for post-processing
-  const postScene = useMemo(() => new Scene(), []);
-  postScene.background = new Color("rgb(255, 255, 255)");
-
-  // Create a new orthographic camera
-  const postCamera = useMemo(
-    () => new OrthographicCamera(-1, 1, 1, -1, 0, 1),
-    []
-  );
-
-  // Create the shader materials
-  const shaderMaterial = useMemo(
-    () =>
-      new RawShaderMaterial({
-        vertexShader: renderer,
-        fragmentShader: fragmentShaderSource,
-        uniforms: {
-          tDiffuse0x: { value: renderTargetRef.current.texture[0] },
-          tDiffuse1x: { value: renderTargetRef.current.texture[1] },
-          tDiffuse2x: { value: renderTargetRef.current.texture[2] },
-          weightsZero: { value: weightsTexZero },
-          weightsOne: { value: weightsTexOne },
-          weightsTwo: { value: weightsTexTwo },
-        },
-        glslVersion: GLSL3,
-      }),
-    []
-  );
-
-  // Create a plane geometry and add it to the scene
-  useEffect(() => {
-    const planeMesh = new Mesh(new PlaneGeometry(2, 2), shaderMaterial);
-    postScene.add(planeMesh);
-  }, [shaderMaterial, postScene]);
-
-  // In the render loop, render the post-processing scene
-  useFrame(() => {
-    gl.setRenderTarget(renderTargetRef.current);
-    gl.clearColor();
-    gl.clearDepth();
-    gl.autoClear = false;
-    gl.render(scene, camera);
-
-    gl.setRenderTarget(null);
-    gl.clear();
-    gl.render(postScene, postCamera);
-  });
-
-  return null;
-}
-
-const MyScene = ({ objFiles, pngFiles, params, renderTargetRef }) => {
-  const { size } = useThree();
-  if (!renderTargetRef.current) {
-    renderTargetRef.current = new WebGLMultipleRenderTargets(
-      size.width * 2,
-      size.height * 2,
-      3
-    );
-    for (let i = 0, il = renderTargetRef.current.texture.length; i < il; i++) {
-      renderTargetRef.current.texture[i].minFilter = LinearFilter;
-      renderTargetRef.current.texture[i].magFilter = LinearFilter;
-      renderTargetRef.current.texture[i].type = FloatType;
-    }
-  }
-  useEffect(() => {
-    renderTargetRef.current.setSize(size.width * 2, size.height * 2);
-  }, [size]);
-
-  const objs = objFiles.map((path) => useLoader(OBJLoader, path));
-  const textures = pngFiles.map((path) => {
-    const texture = new TextureLoader().load(path);
-    texture.magFilter = NearestFilter;
-    texture.minFilter = NearestFilter;
-    return texture;
-  });
-
-  const uniforms = textures.reduce((acc, curr, idx) => {
-    acc[`tDiffuse${idx}`] = { value: curr };
-    return acc;
-  }, {});
-
-  let newmat = new RawShaderMaterial({
-    side: DoubleSide,
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    uniforms,
-    glslVersion: GLSL3,
-  });
-
-  const group = new Group();
-  objs.forEach((model) => {
-    if (model.children.length > 0) {
-      group.add(model.children[0]);
-    }
-  });
-  group.children.forEach((child) => {
-    if (child instanceof Mesh && params.feature) {
-      child.material = newmat;
-    }
-  });
-
-  return (
-    <group>
-      <primitive object={group} />
-    </group>
-  );
-};
+import Viewer from "./views/viewer";
 
 const imageList = [
   { id: 1, src: "https://picsum.photos/200", title: "Image 1" },
@@ -177,7 +30,6 @@ const imageList = [
 ];
 
 function App() {
-  const renderTargetRef = useRef();
   const [objFiles, setObjFIles] = useState(null);
   const [pngFiles, setPngFIles] = useState(null);
   const [jsonFile, setJsonFile] = useState(null);
@@ -235,32 +87,12 @@ function App() {
         </div>
         <div className="main-content">
           <div className="canvas">
-            <Canvas
-              gl={{
-                powerPreference: "high-performance",
-                precision: "highp",
-                antialias: !params.aliasing,
-                pixelRatio: 1,
-              }}
-              onCreated={({ gl }) => {
-                gl.setClearColor(new Color("rgb(0, 0, 0)"), 0.5);
-              }}
-            >
-              <ambientLight />
-              <pointLight position={[10, 10, 10]} />
-              <MyScene
-                objFiles={objFiles}
-                pngFiles={pngFiles}
-                params={params}
-                renderTargetRef={renderTargetRef}
-              />
-              <OrbitControls autoRotate enableZoom={false} enablePan={false} />
-              <PostProcessingScene
-                jsonFile={jsonFile}
-                params={params}
-                renderTargetRef={renderTargetRef}
-              />
-            </Canvas>
+            <Viewer
+              objFiles={objFiles}
+              pngFiles={pngFiles}
+              jsonFile={jsonFile}
+              params={params}
+            />
           </div>
           <div className="controls">
             <FormControl component="fieldset">
---
+++ src/views/viewer.js
@@ -0,0 +1,183 @@
+import React, { useEffect, useRef, useMemo } from "react";
+import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { useLoader } from "@react-three/fiber";
+import {
+  Group,
+  TextureLoader,
+  RawShaderMaterial,
+  DoubleSide,
+  GLSL3,
+  Mesh,
+  NearestFilter,
+  OrthographicCamera,
+  Scene,
+  PlaneGeometry,
+  Color,
+  WebGLMultipleRenderTargets,
+  LinearFilter,
+  FloatType,
+} from "three";
+import { OrbitControls } from "@react-three/drei";
+import { vertexShader, fragmentShader, renderer } from "../components/shaders";
+import createViewDependenceFunctions from "../components/createViewDependence";
+import createNetworkWeightTexture from "../components/createNetworkWeightTexture";
+
+extend({ OBJLoader });
+
+function PostProcessingScene({ jsonFile, params, renderTargetRef }) {
+  const { gl, scene, camera } = useThree();
+  const network_weights = jsonFile;
+  const fragmentShaderSource = createViewDependenceFunctions(network_weights);
+  const weightsTexZero = createNetworkWeightTexture(
+    network_weights["0_weights"]
+  );
+  const weightsTexOne = createNetworkWeightTexture(
+    network_weights["1_weights"]
+  );
+  const weightsTexTwo = createNetworkWeightTexture(
+    network_weights["2_weights"]
+  );
+
+  // Create a new scene for post-processing
+  const postScene = useMemo(() => new Scene(), []);
+  postScene.background = new Color("rgb(255, 255, 255)");
+
+  // Create a new orthographic camera
+  const postCamera = useMemo(
+    () => new OrthographicCamera(-1, 1, 1, -1, 0, 1),
+    []
+  );
+
+  // Create the shader materials
+  const shaderMaterial = useMemo(
+    () =>
+      new RawShaderMaterial({
+        vertexShader: renderer,
+        fragmentShader: fragmentShaderSource,
+        uniforms: {
+          tDiffuse0x: { value: renderTargetRef.current.texture[0] },
+          tDiffuse1x: { value: renderTargetRef.current.texture[1] },
+          tDiffuse2x: { value: renderTargetRef.current.texture[2] },
+          weightsZero: { value: weightsTexZero },
+          weightsOne: { value: weightsTexOne },
+          weightsTwo: { value: weightsTexTwo },
+        },
+        glslVersion: GLSL3,
+      }),
+    []
+  );
+
+  // Create a plane geometry and add it to the scene
+  useEffect(() => {
+    const planeMesh = new Mesh(new PlaneGeometry(2, 2), shaderMaterial);
+    postScene.add(planeMesh);
+  }, [shaderMaterial, postScene]);
+
+  // In the render loop, render the post-processing scene
+  useFrame(() => {
+    gl.setRenderTarget(renderTargetRef.current);
+    gl.clearColor();
+    gl.clearDepth();
+    gl.autoClear = false;
+    gl.render(scene, camera);
+
+    gl.setRenderTarget(null);
+    gl.clear();
+    gl.render(postScene, postCamera);
+  });
+
+  return null;
+}
+
+const MyScene = ({ objFiles, pngFiles, params, renderTargetRef }) => {
+  const { size } = useThree();
+  if (!renderTargetRef.current) {
+    renderTargetRef.current = new WebGLMultipleRenderTargets(
+      size.width * 2,
+      size.height * 2,
+      3
+    );
+    for (let i = 0, il = renderTargetRef.current.texture.length; i < il; i++) {
+      renderTargetRef.current.texture[i].minFilter = LinearFilter;
+      renderTargetRef.current.texture[i].magFilter = LinearFilter;
+      renderTargetRef.current.texture[i].type = FloatType;
+    }
+  }
+  useEffect(() => {
+    renderTargetRef.current.setSize(size.width * 2, size.height * 2);
+  }, [size]);
+
+  const objs = objFiles.map((path) => useLoader(OBJLoader, path));
+  const textures = pngFiles.map((path) => {
+    const texture = new TextureLoader().load(path);
+    texture.magFilter = NearestFilter;
+    texture.minFilter = NearestFilter;
+    return texture;
+  });
+
+  const uniforms = textures.reduce((acc, curr, idx) => {
+    acc[`tDiffuse${idx}`] = { value: curr };
+    return acc;
+  }, {});
+
+  let newmat = new RawShaderMaterial({
+    side: DoubleSide,
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms,
+    glslVersion: GLSL3,
+  });
+
+  const group = new Group();
+  objs.forEach((model) => {
+    if (model.children.length > 0) {
+      group.add(model.children[0]);
+    }
+  });
+  group.children.forEach((child) => {
+    if (child instanceof Mesh && params.feature) {
+      child.material = newmat;
+    }
+  });
+
+  return (
+    <group>
+      <primitive object={group} />
+    </group>
+  );
+};
+
+function Viewer({ objFiles, pngFiles, jsonFile, params }) {
+  const renderTargetRef = useRef();
+  return (
+    <Canvas
+      gl={{
+        powerPreference: "high-performance",
+        precision: "highp",
+        antialias: !params.aliasing,
+        pixelRatio: 1,
+      }}
+      onCreated={({ gl }) => {
+        gl.setClearColor(new Color("rgb(0, 0, 0)"), 0.5);
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <MyScene
+        objFiles={objFiles}
+        pngFiles={pngFiles}
+        params={params}
+        renderTargetRef={renderTargetRef}
+      />
+      <OrbitControls autoRotate enableZoom={false} enablePan={false} />
+      <PostProcessingScene
+        jsonFile={jsonFile}
+        params={params}
+        renderTargetRef={renderTargetRef}
+      />
+    </Canvas>
+  );
+}
+
+export default Viewer;
